166f16c3cad7fe0169fe1ef899a639c02994ebf9
liuz <liuzhuan@qingsongchou.com>
2017-11-06 14:39:00 +0800

    add bug #1

--- test-cases/ios-input-empty/demo/pages/index.js
+++ test-cases/ios-input-empty/demo/pages/index.js
@@ -1,1 +1,33 @@
-Page({})
+Page({
+    data: {
+        SDKVersion: '',
+        model: '',
+        system: '',
+        version: '',
+        platform: '',
+    },
+    onLoad,
+})
+
+function onLoad() {
+    wx.getSystemInfo({
+        success: (res) => {
+            console.log(res)
+            const {
+                SDKVersion, 
+                model, 
+                system, 
+                version, 
+                platform
+            } = res
+
+            this.setData({
+                SDKVersion,
+                model,
+                system,
+                version,
+                platform,
+            })
+        }
+    })
+}
